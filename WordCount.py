656bb0175f353301af8de3bb6682d336cc661c73
Melica <melicampandi@outlook.com>
2022-05-29 22:20:46 +0100

    Adding others files  from anaother project

--- WordCount.py
+++ WordCount.py
@@ -1,6 +1,6 @@
-Mytext = "I love programming and to program."
+Mytext = input("Enter a sentence: ")
 length_sentence = len(Mytext)
-words_count = len(Mytext.split())
+words_count = len(Mytext.split(" "))
 print("Number of words in my sentence :", words_count)
 print("length of my sentence :", length_sentence)
 
